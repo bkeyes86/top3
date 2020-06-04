a32899c9ab97541a5cc2798124241a29c36b2cd1
bkeyes86 <byronvkeyes@gmail.com>
2020-06-04 13:49:02 -0500

    trying to make prompts work

--- js/app.js
+++ js/app.js
@@ -1,9 +1,30 @@
+('use strict');
 
+console.log('This file is not connected!');
+console.log('This file is not connected')
+var bestBand = prompt('enter your favorite band!');
 
-console.log('This file connected!');
-var bestBand = prompt('enter your pick!');
+var guess = prompt('guess where ed sheeran is on the list 1-3');
 //variable declaration
+
+if (guess >= 1 && guess > 3){
+    greeting = 'good job, ' + bestBand+ ' !';
+} else if (guess < 1 &&  > 3){
+    greeting = `almost ${bestBand}`' !';
+} else if (guess >= 3 && guess <= 3) {
+    greeting = ' nope ' + bestBand;
+} else {
+    greeting = 'rockstar ';
+}
+
+return greeting '+' guess
+}
+
+console.log(`${greeting} ${guess} ${bestBand}`);
+
 console.log(bestBand);
+
 document.write(bestBand);
+document.write(guess);
 
 
