c89468b1937881af5aac03ad6a84e0fd17dfea8d
bkeyes86 <byronvkeyes@gmail.com>
2020-06-04 14:06:57 -0500

    prompts not functioning

--- js/app.js
+++ js/app.js
@@ -1,11 +1,14 @@
 ('use strict');
 
+
 console.log('This file is connected!');
+function helloMsg(){
 
 var bestBand = prompt('enter your favorite band!');
 
 var guess = prompt('guess where ed sheeran is on the list 1-3');
 //variable declaration
+
 
 if (guess >= 1 && guess > =1){
     greeting = 'good job, ' + bestBand+ ' !';
