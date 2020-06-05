331d2c3d8963ad719ab8ab1f5a3cbe591a8dd556
bkeyes86 <byronvkeyes@gmail.com>
2020-06-04 23:24:18 -0500

    hw need js fixed

--- js/app.js
+++ js/app.js
@@ -33,4 +33,21 @@ console.log(result);
 document.write(result);
 
 
+for (var i = 0; i < 3; i++) {
+    document.write(i);
+}
 
+    var Tickets = function() {
+    var order = prompt('which show do you wanna see?');
+    var tix;
+
+    while(order !== 'ed' && order !== 'U2' ) {
+        order = prompt ('pick "ed" or "U2"...');
+    }
+    if(order === 'ed') {
+        tix = '<img src="images/574342e4d76b607bbd475efbdad8b50c.jpg">';
+    }else if (order === 'U2') {
+        tix = '<img src="images/ok.jpg">';
+    }
+    return tix;
+}
